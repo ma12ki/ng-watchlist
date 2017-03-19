90e81658a6ec0276f560a1b03835743da9e4c875
mzolkiewski <zolkiewski.marcin@gmail.com>
2017-03-19 11:45:32 +0100

    selecting only episodes that have not been watched by the user

--- server/src/schema/modules/shows/available-episodes.resolver.ts
+++ server/src/schema/modules/shows/available-episodes.resolver.ts
@@ -1,12 +1,24 @@
 import * as moment from 'moment';
 
-import { EpisodeModel } from "./models/episode.model";
+import { EpisodeModel } from './models/episode.model';
+import { UserEpisodeModel } from './models/user-episode.model';
 
-const availableEpisodes = async () => {
+const availableEpisodes = async (_root, _args, {user}) => {
   const now = moment().toISOString();
 
+  const userEpisodes = await UserEpisodeModel.find({
+    userId: user._id,
+  }).select({
+    episodeId: 1,
+  }).lean() as Array<any>;
+
+  const userEpisodeIds = userEpisodes.map((ep) => ep.episodeId);
+
   return EpisodeModel.find({
+    _id: { $nin: userEpisodeIds },
     premiereDate: { $lte: now },
+  }).sort({
+    premiereDate: 1,
   }).lean();
 };
 
