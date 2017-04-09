6ff4012615d0701c2c7a5223f05ed9909ecaad5a
mzolkiewski <zolkiewski.marcin@gmail.com>
2017-04-09 15:16:34 +0200

    updated permission service logic

--- common/users/permission.service.ts
+++ common/users/permission.service.ts
@@ -1,34 +1,34 @@
 import { UserRole } from './role.enum';
 import { IUser } from './user.interface';
 
-const getRoles = (user: IUser) => {
-  return user.roles;
-};
-
-const hasRole = (user: IUser, role: UserRole) => {
-  return (<any>getRoles(user)).includes(role);
-};
-
-const hasAdminRole = (user: IUser) => {
-  return hasRole(user, UserRole.Admin);
-};
-
-const hasUserRole = (user: IUser) => {
-  return hasRole(user, UserRole.User);
-};
-
 const isExpired = (user: IUser) => {
-  return new Date(user.expirationDate) < new Date();
+  return !user || new Date(user.expirationDate) < new Date();
 };
 
 const isValid = (user: IUser) => {
   return user && !isExpired(user);
 };
 
+const getRoles = (user: IUser) => {
+  return user.roles;
+};
+
+const hasRole = (user: IUser, role: UserRole) => {
+  return isValid(user) && (<any>getRoles(user)).includes(role);
+};
+
+const hasAdminRole = (user: IUser) => {
+  return isValid(user) && hasRole(user, UserRole.Admin);
+};
+
+const hasUserRole = (user: IUser) => {
+  return isValid(user) && hasRole(user, UserRole.User);
+};
+
 export const permissionService = {
+  isValid,
+  isExpired,
   hasRole,
   hasAdminRole,
   hasUserRole,
-  isExpired,
-  isValid,
 };
