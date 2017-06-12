159ee3c92d2c27195860946ddf012712d9b1c3e6
mzolkiewski <zolkiewski.marcin@gmail.com>
2017-06-12 21:02:43 +0200

    added error info for all shows list

--- client/src/app/shows/all-shows/all-shows/all-shows.component.ts
+++ client/src/app/shows/all-shows/all-shows/all-shows.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { Router } from '@angular/router';
+import { select } from '@angular-redux/store';
+
+import * as allShowsSelectors from '../all-shows.selectors';
 
 @Component({
   selector: 'wl-all-shows',
@@ -7,14 +10,13 @@ import { Router } from '@angular/router';
   styleUrls: ['./all-shows.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AllShowsComponent implements OnInit {
+export class AllShowsComponent {
+  @select(allShowsSelectors.error) error$;
+  @select(allShowsSelectors.isFetching) isFetching$;
 
   constructor(
     private router: Router,
   ) { }
-
-  ngOnInit() {
-  }
 
   navigateToNew() {
     this.router.navigateByUrl('shows/new');
